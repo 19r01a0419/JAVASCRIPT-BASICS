4cdff1882cf55c0e99ec3b460d76b107a3fb830f
D.ChennuBhavani <101263204+19r01a0419@users.noreply.github.com>
2023-06-04 14:15:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var A=8;
+console.log(A);
+// constant
+const pi=3.1415;
+console.log(pi);
+//  arithmetic 
+let n1=6;
+let n2=8;
+let result=n1+n2;
+console.log(result);
+// assignment operators
+let s=3;
+let d="3";
+let v=(s==d);
+console.log(v);
+// operators
+let number=4 + 5 * 10 - (52 + 4);
+console.log(number);
+// data types -string
+let Q="2";
+console.log(typeof(Q));
+// 
+let d1="chennu";
+let d2=3;
+let d3=9;
+let r=(d1+d2+d3);
+console.log(r);
+let R=(d2+d3+d1);
+console.log(R);
+// conditional statements
+let n=6;
+
+if(n%2==0){
+  console.log("2 is a factor of that number");}
+else{
+  console.log("it is not a factor of that number");}
+//   loops
+let i=4;
+for(i=4;i<=40;i++){
+    console.log(i);
+}
+
+let f=0;
+while(f<=10)
+{
+    console.log(f*10);
+    f++;
+}
+
+
+
+
+
